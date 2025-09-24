659cbbcbad4e4f70ce92f85daeaf077cf0a183fb
Gab1trs <torresgabin91@gmail.com>
2025-09-24 13:02:45 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
-title = "Cross Asset REgime Monitor"
+title = "Cross Asset Regime Monitor"
 st.set_page_config(page_title=title, layout="wide")
 st.title(title)
-# data=pd.read_csv('all_assets.csv', index_col=0, parse_dates=True)
-# data=data.ffill().dropna()
-# returns=data.pct_change()
-# cum_returns=(1+returns).cumprod()
 
-# st.dataframe(cum_returns)
+data=pd.read_csv('all_assets.csv', index_col=0, parse_dates=True)
+data=data.ffill().dropna()
+returns=data.pct_change()
+cum_returns=(1+returns).cumprod()
+assets=cum_returns.columns
+
+with st.sidebar:
+    selected_assets=st.multiselect("Select Assets", assets)
+    min_date, max_date=st.date_input("Select Date Range", [data.index.min(), data.index.max()])
+
+fig1=plt.figure(figsize=(10,6))
+plt.plot(cum_returns.index, label=cum_returns.columns)
+plt.legend()
+plt.xlabel("Date")
+plt.ylabel("Cumulative Returns")
+plt.title("Cumulative Returns of All Assets")
+
+
+
