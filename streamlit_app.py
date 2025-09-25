7ede1ffd8dfc346f14fb4933847be6c7a4ec30cf
Gab1trs <torresgabin91@gmail.com>
2025-09-25 14:05:22 +0200

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,21 +7,25 @@ st.set_page_config(page_title=title, layout="wide")
 st.title(title)
 
 data=pd.read_csv('all_assets.csv', index_col=0, parse_dates=True)
-data=data.ffill().dropna()
-returns=data.pct_change()
-cum_returns=(1+returns).cumprod()
-assets=cum_returns.columns
+assets=data.columns
 
 with st.sidebar:
     selected_assets=st.multiselect("Select Assets", assets)
     min_date, max_date=st.date_input("Select Date Range", [data.index.min(), data.index.max()])
 
-fig1=plt.figure(figsize=(10,6))
-plt.plot(cum_returns.index, label=cum_returns.columns)
-plt.legend()
-plt.xlabel("Date")
-plt.ylabel("Cumulative Returns")
-plt.title("Cumulative Returns of All Assets")
+# Filter data by date and selected assets
+filtered_data = data.loc[min_date:max_date, selected_assets] if selected_assets else pd.DataFrame()
 
+if not filtered_data.empty:
+    fig1=plt.figure(figsize=(10,6))
+    plt.plot(filtered_data)
+    plt.legend(selected_assets)
+    plt.xlabel("Date")
+    plt.ylabel("Cumulative Returns")
+    plt.title("Cumulative Returns of All Assets")
+    plt.show()
+    st.pyplot(fig1)
 
+else:
+    st.write("Please select at least one asset to display the chart.")
 
